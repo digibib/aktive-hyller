0fa884c60e259badfe2992479cf8db561ad43e53
bensinober <bensinober@gmail.com>
2012-09-18 11:44:01 +0200

    fixed Capfile and deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,56 +1,34 @@
-#========================
-#CONFIG
-#========================
-default_run_options[:pty] = true
-set :domain, "aktivehyller.deichman.no"
 set :application, "aktive-hyller"
+set :repository,  "https://bensinober@github.com/digibib/#{application}.git"
+
 set :scm, :git
-set :git_enable_submodules, 1
-set :repository, "https://bensinober@github.com/digibib/#{application}.git"
-set :branch, "develop"
-set :git_shallow_clone, 1
-set :ssh_options, { :forward_agent => true }
-set :stage, :production
-set :user, "deploy"
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :user, 'deploy'
 set :use_sudo, false
-set :runner, "deploy"
-set :deploy_to, "/var/www/#{application}"
+set :branch, 'develop'
 set :deploy_via, :remote_cache
-set :app_server, :passenger
-set :keep_releases, 5  # How many fallback releases are kept in :deploy_to/releases
-# RVM integration
-set :rvm_ruby_string, 'ruby-1.9.3-p194'
-set :rvm_type, :system
-#========================
-#ROLES
-#========================
-server "171.23.133.229", :app, :web, :db, :primary => true
-#========================
-#CUSTOM
-#========================
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  # Assumes you are using Passenger
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
- 
-  task :finalize_update, :except => { :no_release => true } do
-    run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
- 
-    # mkdir -p is making sure that the directories are there for some SCM's that don't save empty folders
-    run <<-CMD
-      rm -rf #{latest_release}/log &&
-      mkdir -p #{latest_release}/public &&
-      mkdir -p #{latest_release}/tmp &&
-      ln -s #{shared_path}/log #{latest_release}/log
-    CMD
- 
-    if fetch(:normalize_asset_timestamps, true)
-      stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
-      asset_paths = %w(images css).map { |p| "#{latest_release}/public/#{p}" }.join(" ")
-      run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
-    end
-  end
-end                           # Bundler integration
+set :keep_releases, 5     # How many fallback releases are kept in :deploy_to/releases
+
+role :web, "aktivehyller.deichman.no"                          # Your HTTP server, Apache/etc
+role :app, "aktivehyller.deichman.no"                          # This may be the same as your `Web` server
+server "171.23.133.229", :app, :web, :primary => true
+set :deploy_to, "/var/www/#{application}"
+
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
