6a9483dea4fd9fcf7c4e686dd40bcf2c3b341de7
bensinober <bensinober@gmail.com>
2013-02-26 09:17:53 +0100

    added screensaver video

--- xscreensaver-timeout.sh
+++ xscreensaver-timeout.sh
@@ -3,7 +3,10 @@
 process() {
 while read input; do 
   case "$input" in
-    BLANK*)     (echo 'GET /timeout ';sleep 1) | telnet localhost 4567 ;;
+    BLANK*)     
+      (echo 'GET /timeout ';sleep 1) | telnet localhost 4567 
+      [ -f final1.mp4 ] && avplay -fs -loop 0 final1.mp4 # screensaver in full screen loop
+      ;;
     UNBLANK*)	echo "start something? " ;;
     LOCK*)	echo "lock .... do nothing yet" ;;
   esac
