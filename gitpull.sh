1c78ddb486692fee484451b0048473b5e443b6c8
bensinober <bensinober@gmail.com>
2013-03-18 16:11:39 +0100

    gitpull fix

--- gitpull.sh
+++ gitpull.sh
@@ -2,9 +2,9 @@
 # check for github updates, master branch
 source ~/.rvm/scripts/rvm
 cd ~/code/aktive-hyller
-echo "# aktive-hyller update:\n" >> logs/update.log 2>&1 
-git stash && git pull >> logs/update.log 2>&1
-rake configure >> logs/update.log 2>&1
-echo "# rfidgeek update:\n" >> logs/update.log 2>&1 
+echo "# aktive-hyller update:" >> ~/code/aktive-hyller/logs/update.log 2>&1 
+git stash && git pull >> ~/code/aktive-hyller/logs/update.log 2>&1
+rake configure >> ~/code/aktive-hyller/logs/update.log 2>&1
+echo "# rfidgeek update:" >> ~/code/aktive-hyller/logs/update.log 2>&1 
 cd ~/code/rfidgeek
-git stash && git pull >> logs/update.log 2>&1 
+git stash && git pull >> ~/code/aktive-hyller/logs/update.log 2>&1 
