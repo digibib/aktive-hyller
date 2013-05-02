f51c9035a3a3d17f200cf16d37f2c93e9fc8c3d9
bensinober <bensinober@gmail.com>
2013-05-02 14:40:23 +0200

    xscreensaver timeout fix

--- scripts/xscreensaver-timeout.sh
+++ scripts/xscreensaver-timeout.sh
@@ -4,7 +4,7 @@ process() {
 while read input; do 
   case "$input" in
     BLANK*)    (sleep 1 ; echo 'GET /timeout ') | telnet localhost 4567 
-                /usr/bin/firefox --display=localhost:0.0 -remote "openurl(localhost:4567)" ;;
+                /usr/bin/firefox --display=:0.0 -remote "openurl(localhost:4567)" ;;
     UNBLANK*)	  echo "start something? " ;;
     LOCK*)	    echo "lock .... do nothing yet" ;;
   esac
