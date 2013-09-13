569e1a3ce9941552233f4c6d004f994bb755c172
Asgeir Rekkavik <asgeir@asgeir-Latitude-E4310.(none)>
2013-09-13 15:25:20 +0200

    added tests for Goodreads

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -13,11 +13,23 @@ describe "Book" do
 	describe "Goodreads" do
 		let(:response) { fake_response(File.open('spec/goodreads.xml').read) }
 
-		context "Rating" do
+		context "Goodreads response" do
+
+			before {book.Goodreads(response)}
+
 			it "saves source of rating" do
-				book.Goodreads(response)
 				book.rating[:source].should == "Goodreads"
 			end
+			it "saves description" do
+				book.review_collection.should include {|r| r[:source] == "Goodreads"} 
+			end
+			it "saves sum of ratings" do
+				book.rating[:rating].should == 596
+			end
+			it "saves number of ratings" do
+				book.rating[:num_raters].should == 159
+			end
+
 		end
 
 	end
