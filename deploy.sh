95d614088f70728181f37d05e43381ba1e020a10
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-09-13 16:21:32 +0200

    now try this

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh $1 "cd /home/aktiv/code/aktive-hyller; git pull; source /home/aktiv/.bashrc; source /home/aktiv/.bash_profile; bundle; pkill firefox; sudo service aktivehyller restart"
+ssh $1 "cd /home/aktiv/code/aktive-hyller; git pull; rvm use 1.9.3; bundle; pkill firefox; sudo service aktivehyller restart"
