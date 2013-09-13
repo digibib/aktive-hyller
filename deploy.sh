af490ee51cda61bbb9f3b115ff8b2bd5d93c7615
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-09-13 16:08:07 +0200

    source .bashrc

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh $1 "cd /home/aktiv/code/aktive-hyller; git pull; source ~/home/aktiv/.profile; bundle; pkill firefox; sudo service aktivehyller restart"
+ssh $1 "cd /home/aktiv/code/aktive-hyller; git pull; source ~/home/aktiv/.bashrc; bundle; pkill firefox; sudo service aktivehyller restart"
