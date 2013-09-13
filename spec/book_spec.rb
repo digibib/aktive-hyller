736c7d065a054224575ce6d434299ca19a4aa703
Asgeir Rekkavik <asgeir@asgeir-Latitude-E4310.(none)>
2013-09-13 15:39:29 +0200

    added tests for Bokkilden

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -10,27 +10,37 @@ describe "Book" do
 		Typhoeus::Response.new(code: 200, body: body)
 	end
 
-	describe "Goodreads" do
+	context "Goodreads" do
 		let(:response) { fake_response(File.open('spec/goodreads.xml').read) }
 
-		context "Goodreads response" do
+		before {book.Goodreads(response)}
 
-			before {book.Goodreads(response)}
-
-			it "saves source of rating" do
-				book.rating[:source].should == "Goodreads"
-			end
-			it "saves description" do
-				book.review_collection.should include {|r| r[:source] == "Goodreads"} 
-			end
-			it "saves sum of ratings" do
-				book.rating[:rating].should == 596
-			end
-			it "saves number of ratings" do
-				book.rating[:num_raters].should == 159
-			end
-
+		it "saves source of rating" do
+			book.rating[:source].should == "Goodreads"
 		end
-
+		it "saves description" do
+			book.review_collection.should include {|r| r[:source] == "Goodreads"} 
+		end
+			it "saves sum of ratings" do
+			book.rating[:rating].should == 596
+		end
+		it "saves number of ratings" do
+			book.rating[:num_raters].should == 159
+		end
 	end
+
+	context "Bokkilden" do
+		let (:response) {fake_response(File.open('spec/bokkilden.xml').read)}
+
+		before {book.Goodreads(response)}
+
+		it "saves book review" do
+			book.review_collection.should include {|r| r[:source] == "Bokkilden"}
+			book.review_collection.should include {|r| r[:text] == "Dette er den niende boken om William Wisting."}
+		end
+	end
+
+
+
+
 end
